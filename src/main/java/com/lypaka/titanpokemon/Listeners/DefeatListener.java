7f916c5f4bcafe4074d5dd5782135fe43854c953
Lypaka <ryan96t@gmail.com>
2023-12-27 13:18:57 -0600

    Removed some debug messages

--- src/main/java/com/lypaka/titanpokemon/Listeners/DefeatListener.java
+++ src/main/java/com/lypaka/titanpokemon/Listeners/DefeatListener.java
@@ -17,15 +17,12 @@ public class DefeatListener {
         ServerPlayerEntity player = event.player;
         Pokemon pokemon = event.wpp.controlledPokemon.get(0).pokemon;
 
-        System.out.println("hi");
         if (TitanHandler.isTitanPokemon(pokemon)) {
 
-            System.out.println("hi 2");
             Titan titan = TitanHandler.getFromPokemon(pokemon);
             // Safety check
             if (titan == null) return;
 
-            System.out.println("hi 3");
             int titanLevel = TitanHandler.getTitanLevel(pokemon);
             TitanDefeatEvent defeatEvent = new TitanDefeatEvent(player, titan, titanLevel);
             MinecraftForge.EVENT_BUS.post(defeatEvent);
